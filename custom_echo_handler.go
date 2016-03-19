1c3c7a86048b63ab73fe328823c5ff1e4dc3da0e
Marcin Tojek <tojek.m@gmail.com>
2016-03-19 15:45:54 +0100

    echopprof adapted to both fasthttp and standard engine

--- custom_echo_handler.go
+++ custom_echo_handler.go
@@ -3,25 +3,41 @@ package echopprof
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/engine/fasthttp"
 	"net/http"
+	"log"
+	"sync"
 )
 
 type customEchoHandler struct {
-	handlerFunc func(echo.Context) error
+	httpHandler http.Handler
+
+	wrappedHandleFunc echo.HandlerFunc
+	once sync.Once
 }
 
 func (ceh *customEchoHandler) Handle(c echo.Context) error {
-	return ceh.handlerFunc(c)
+	ceh.once.Do(func() {
+		ceh.wrappedHandleFunc = ceh.mustWrapHandleFunc(c)
+	})
+	return ceh.wrappedHandleFunc(c)
+}
+
+func (ceh *customEchoHandler) mustWrapHandleFunc(c echo.Context) echo.HandlerFunc {
+	if _, ok := c.Request().(*standard.Request); ok {
+		return standard.WrapHandler(ceh.httpHandler)
+	} else if _, ok = c.Request().(*fasthttp.Request); ok {
+		return NewFastHTTPEchoAdaptor(ceh.httpHandler)
+	}
+
+	log.Fatal("Unknown HTTP implementation")
+	return nil
 }
 
 func fromHTTPHandler(httpHandler http.Handler) *customEchoHandler {
-	return &customEchoHandler{
-		handlerFunc: standard.WrapHandler(httpHandler),
-	}
+	return &customEchoHandler{ httpHandler: httpHandler }
 }
 
 func fromHandlerFunc(serveHTTP func(w http.ResponseWriter, r *http.Request)) *customEchoHandler {
-	return &customEchoHandler{
-		handlerFunc: standard.WrapHandler(&customHTTPHandler{serveHTTP: serveHTTP}),
-	}
+	return &customEchoHandler{ httpHandler: &customHTTPHandler{ serveHTTP: serveHTTP }}
 }
